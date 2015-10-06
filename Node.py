4ed3051a2020f3e5c05a07d05ef636adc3b89647
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2015-10-06 11:47:33 +0000

    Node.py edited online with Bitbucket

--- Node.py
+++ Node.py
@@ -1,9 +1,17 @@
 class Adyacent_Node:
   key = 0
   distance = 0
-  '''estamos trabajando en ello'''
+  
+  def __init__(self, k, d):
+    self.key = k
+    self.distance = d
+  
+  def toString(self):
+    return str(self.key) + " " + str(self.distance)
+
   
 class Node:
+  
   latitud = 0
   longitud = 0
   ady_list = []
@@ -14,11 +22,16 @@ class Node:
     
   def add_node(self, numb):
     self.ady_list.append(numb)
+
+  def toString(self):
+    return str(self.latitud) + " " + str(self.longitud)
+
     
 nodo = {}
 
 nodo['1234'] = Node(3,4)  
 
-nodo['1234'].add_node(10)
+nodo['1234'].add_node(Adyacent_Node(2,3))
   
-print nodo['1234'].ady_list
+print nodo['1234'].ady_list #imprime el puntero a memoria, no los objetos 
+print nodo['1234'].toString()
