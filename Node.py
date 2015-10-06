aaa00d4c94384ba8ba0c6fe1fb5d0215abc8f5bc
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2015-10-06 11:14:21 +0000

    Clase Nodo

---
+++ Node.py
@@ -0,0 +1,13 @@
+class Node:
+  latitud = 0
+  longitud = 0
+  ady_list = {}
+  
+  def __init__(self, lat, lon)
+    self.latitud = lat
+    self.longitud = lon
+    
+  def funcion(self)
+    print self.latitud
+    
+    
