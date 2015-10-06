51bbb03cf770ac98c95bad00e2a0a657d904ec67
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2015-10-06 11:31:57 +0000

    Node.py edited online with Bitbucket

--- Node.py
+++ Node.py
@@ -1,13 +1,24 @@
+class Adyacent_Node:
+  key = 0
+  distance = 0
+  '''estamos trabajando en ello'''
+  
 class Node:
   latitud = 0
   longitud = 0
-  ady_list = {}
+  ady_list = []
   
-  def __init__(self, lat, lon)
+  def __init__(self, lat, lon):
     self.latitud = lat
     self.longitud = lon
     
-  def funcion(self)
-    print self.latitud
+  def add_node(self, numb):
+    self.ady_list = self.ady_list.append(numb)
     
+  
+x = Node(1,2)
+
+x.add_node(3) #imprime None, que ni significa 'lista vacia'
+
+print x.ady_list   
     
