a354660d774833d8184d0d801ada4b691180ca38
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2015-10-12 20:44:09 +0000

    alpha_main.py edited online with Bitbucket

--- alpha_main.py
+++ alpha_main.py
@@ -33,11 +33,16 @@ class Node:
         self.ady_list.append(ady_node)
 
     def toString(self):
-        line = ''
-        print self.ady_list[0].toString()
-        for item in self.ady_list:
-            line += (str(item.toString()) + "\n")
-        return "Node #" + str(self.key) + ", latitud: " + str(self.latitud) + ", longitud: " + str(self.longitud) + "\nAdyacent nodes:\n" + line
+        ady_nodes = '\n'
+        
+        if len(self.ady_list)>0:
+            print self.ady_list[0].toString()
+            for item in self.ady_list:
+                ady_nodes += (str(item.toString()) + "\n")
+        else: 
+            ady_nodes = "Empty list"
+                
+        return "Node #" + str(self.key) + ", latitud: " + str(self.latitud) + ", longitud: " + str(self.longitud) + "\nAdyacent nodes: " + ady_nodes
 
 
 ##### SUPPORT #####
@@ -78,7 +83,7 @@ for i in range(0, len(lines) - 1):
     if (lines[i].find("<node") != -1): #guarda cada nodo en la tabla hash 
         tokenized_line = lines[i].split('"')
         n_nodes += 1
-        #print "IDNode: " + tokenized_line[1] + " || Latitud:  " + tokenized_line[15] + " || Longitud:  " + \
+        print "IDNode: " + tokenized_line[1] + " || Latitud:  " + tokenized_line[15] + " || Longitud:  " + \
         tokenized_line[17]
 
         HT[tokenized_line[1]] = Node(tokenized_line[1], tokenized_line[15], tokenized_line[17])
@@ -88,10 +93,11 @@ for i in range(0, len(lines) - 1):
         previous_line = lines[i - 1].split('"')
         current_line = lines[i].split('"')
         tmpList.append((previous_line[1], current_line[1]))
-        #print previous_line[1] + "-->" + current_line[1]
+        print previous_line[1] + "-->" + current_line[1]
         n_conex += 1
     
     
+    #hay errores en las condiciones de este if, no guarda nodos ni ciudades
     if ((we_are_in_ways_section) and (lines[i].find("<tag k=""name"" v=") != -1)): #si estamos en los ways y encuentra una etiqueta nombre
         current_line = lines[i].split('"')
         current_city = current_line[3] #pillamos el nombre de la ciudad de la que hemos guardado los nodos antes
@@ -102,11 +108,11 @@ for i in range(0, len(lines) - 1):
         del tmpList
         tmpList = []
         
-    sys.stdout.write("\r" + str(int(round((float(i)/len(lines))*100))) + "% of the data imported from the .osm file")
+    #sys.stdout.write("\r" + str(int(round((float(i)/len(lines))*100))) + "% of the data imported from the .osm file")
     
 print "Lineas: " + str(i) + " || Nodos: " + str(n_nodes) + " || Conexiones: " + str(n_conex)
 
 
 # impresion de prueba
 # print HT['3753271185'].add_node(Adyacent_Node(2,"mata", 3))
-print HT['803292473'].toString()
+print HT['3753271186'].toString()
