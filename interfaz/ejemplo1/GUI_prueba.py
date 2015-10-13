a13113764c326980fed2d8f7ff370d94b557780d
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2015-10-13 15:30:01 +0000

    GUI_prueba.py created online with Bitbucket

---
+++ interfaz/ejemplo1/GUI_prueba.py
@@ -0,0 +1,32 @@
+try:
+    import gtk
+except:
+    print("GTK Not Available")
+    sys.exit(1)
+
+class GUI_prueba:
+    builder = None
+    window = None
+
+    def __init__(self):
+        gtk.gdk.threads_init()
+        gladefile = "gui_prueba.glade"
+        self.builder = gtk.Builder()
+        self.builder.add_from_file(gladefile)
+        self.completo = False
+        
+        handlers = {
+            "on_button_clicked": self.create_file
+        }
+        
+        self.builder.connect_signals(handlers)
+        self.window = self.builder.get_object("window1")
+        self.window.show()
+        
+    def create_file(self, button):
+        file = open('myfile.txt', 'w+')
+    
+    
+gui = GUI_prueba()
+gtk.main()
+
