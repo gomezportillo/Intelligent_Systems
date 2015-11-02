2b28f34dea55c1ba2a9fc81473bf66b487cb389b
pedroma-gomezp <pedroga_tuenti@hotmail.com>
2015-11-03 00:20:18 +0100

    Añadiendo las clases en archivos distintos

---
+++ milestone2/src/Node_Tree.py
@@ -0,0 +1,23 @@
+from State import State
+from random import randint
+
+class Node_Tree:
+    '''Class repressenting the node object we will be inserting in our frontier'''   
+ 
+    def __init__(self, s, c=0, a=None, d=0, p=None):
+        self.state = State(s)
+        self.cost = c
+        self.value = randint(0,100000)
+        self.action = a
+        self.depth = d
+        self.parent = p
+
+    def __lt__(self, other):
+        if isinstance(other, Node_Tree): 
+            return self.value < other.value
+        else:
+            raise TypeError
+
+    def __repr__(self):
+        return self.state.id_state + " " + str(self.value)
+
