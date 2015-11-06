a653a8b256c48bb07ce255b393e168b48520eace
pedroma-gomezp <pedroga_tuenti@hotmail.com>
2015-11-06 21:49:19 +0100

    borrando

--- milestone2/A_old/src/State_Space.py
+++
@@ -1,8 +0,0 @@
-class State_Space:
-    '''Class repressenting the state space of our problem'''
-    def getSuccessors(self, state, hash_table):
-        return hash_table[str(state.id_state)].ady_list[:]
-
-    def isValid(self, state): pass
-    def isGoal(self, state): pass
-
