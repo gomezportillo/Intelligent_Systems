4f4a2966697264fb847e1f0664ae7a4d8f33683b
pedroma-gomezp <pedroga_tuenti@hotmail.com>
2015-11-14 23:56:09 +0100

    Cambios a medianoche

--- milestone3/src/State_Space.py
+++ milestone3/src/State_Space.py
@@ -1,9 +1,15 @@
 from State import State
+from Node_Map import Node_Map
 
 class State_Space: #hacer un constructor con los limites geograficos del mapa
     '''Class repressenting the state space of our problem'''
 
-    def getSuccessors(self, prev_state, hash_table):#si lo que queremos es un nodo, no tiene sentido que te lo devuelva
+    def __init__(self, boundary_coordinates):
+        self.latitute_range = boundary_coordinates[0:2]
+        self.longitud_range = boundary_coordinates[2:4]
+
+    def getSuccessors(self, prev_state, hash_table):
+        
         successors_list = []
         ady_list = hash_table[prev_state.node_map.key].ady_list[:]
         
@@ -22,5 +28,13 @@ class State_Space: #hacer un constructor con los limites geograficos del mapa
 
         return successors_list
 
-    def isValid(self, state): pass #pps eso
+    def isValid(self, node):
+        
+        if not isinstance(node_map, Node_Map): 
+            raise TypeError                
+        
+        vertical_check = latitude_range[0] < node.latitude and node.latitude < latitude_range[1]
+        horizontal_check = longitud_range[0] < node.longitud and node.longitud < longitud_range[1]
 
+        return vertical_check and horizontal_check
+        
