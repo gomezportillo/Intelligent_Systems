d7b00db52357fa5aa80ea9f0cc113eb64441b2f7
pedroma-gomezp <pedroga_tuenti@hotmail.com>
2015-11-14 18:30:16 +0100

    .

--- milestone3/src/State.py
+++ milestone3/src/State.py
@@ -3,13 +3,16 @@ from Node_Map import Node_Map
 class State:
     '''Class repressenting a state of our problem'''
 
-    def __init__(self, node_map):
+    def __init__(self, node_map, objetive_nodes=None):
 
         if not isinstance(node_map, Node_Map): 
             raise TypeError
+        
+        if objetive_nodes is None:
+            self.objetive_nodes = []
 
-        self.node_map=node_map #map node
-        self.objetive_nodes = [] #list with the ID of the nodes we are looking for
+        self.node_map=node_map
+        self.objetive_nodes = objetive_nodes
 
     def __repr__(self):
         return self.node
