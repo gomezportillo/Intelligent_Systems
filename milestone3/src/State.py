1d61c969e19a105f0bf5ee08f61808bb2005189b
Juan Garrido <juan.garrido3@alu.uclm.es>
2015-11-15 10:54:01 +0000

    Actualizado objetives

--- milestone3/src/State.py
+++ milestone3/src/State.py
@@ -3,13 +3,13 @@ from Node_Map import Node_Map
 class State:
     '''Class repressenting a state of our problem'''
 
-    def __init__(self, node_map):
+    def __init__(self, node_map, objetives):
 
         if not isinstance(node_map, Node_Map): 
             raise TypeError
         
         self.node_map=node_map
-        self.objetive_nodes = []
+        self.objetive_nodes = objetives
 
     def __repr__(self):
         return self.node
